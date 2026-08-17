f2695d08fdca45df7f3e9028117306c99d4f14f7
Douglas Rufino <douglasrufino.23@gmail.com>
2026-08-17 18:20:58 -0300

    feat: adiciona componente ContainerLogin

--- src/components/ContainerLogin.tsx
+++ src/components/ContainerLogin.tsx
@@ -1,24 +1,30 @@
-
+import {User, Lock} from 'lucide-react';
 
 
 export function ContainerLogin() {
     return (
-        <section className="flex flex-col items-center w-8/9 max-w-180 py-10 gap-15 rounded-4xl bg-white  ">
-            <h1 className="text-4xl text-center">
+        <section className="flex flex-col items-center w-[90%] max-w-125 py-10 sm:gap-12 shadow-2xl shadow-black-900/50 rounded-4xl bg-white  ">
+            <h1 className="text-4xl font-bold text-center">
                 Acessar o Sistema
             </h1>
-            <form className="w-1/2 flex flex-col items-center gap-3">
-                <div className="flex flex-col w-3/4 min-w-62.5">
+            <form className="flex flex-col p-6 items-center gap-6">
+                <div className="flex flex-col w-full min-w-62.5 gap-2">
                     <label htmlFor="usuario" className="">Usuário</label>
-                    <input type="text" id="usuario" name="usuario" required className="h-8 px-2 focus:outline-1 rounded-2xl bg-gray-200" />
+                    <div className='flex flex-col relative'>
+                        <User className='absolute top-2 left-1'/>
+                        <input type="text" id="usuario" name="usuario" required className="h-10 px-8 focus:outline-1 rounded-lg bg-gray-200" />
+                    </div>
                 </div>
-                <div className="flex flex-col w-3/4 min-w-62.5">
+                <div className="flex flex-col w-full min-w-62.5">
                     <label htmlFor="senha">Senha</label>
-                    <input type="password" id="senha" name="senha" required className="h-8 px-2 focus:outline-1 rounded-2xl  bg-gray-200"/>
+                    <div className='flex flex-col relative '>
+                        <Lock className='absolute top-2 left-1'/>
+                        <input type="password" id="senha" name="senha" required className="h-10 px-8 focus:outline-1 rounded-lg  bg-gray-200"/>
+                    </div>
                 </div>
-                <button type="submit" className=" p-2 px-3 cursor-pointer rounded-2xl bg-gray-200 hover:bg-gray-300">Entrar</button>
+                <button type="submit" className=" w-full p-2 cursor-pointer rounded-lg bg-blue-400 text-white hover:bg-gray-300">Entrar</button>
             </form>
-            <p className="">Esqueceu a senha? <a href="">Redefinir</a></p>
+            <p className="text-blue-500 font-bold">Esqueceu a senha? <a href="">Redefinir</a></p>
         </section>
 
     );
